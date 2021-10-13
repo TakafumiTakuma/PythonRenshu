806ab67e8772740b125444f1644c772159793fed
TakafumiTakuma <flowesbelieve2136@gmail.com>
2021-10-14 00:34:48 +0900

    startPython

---
+++ index.py
@@ -0,0 +1,27 @@
+# #変数を定義する
+# a = 1 #数値
+# b = 'AAA' #文字列
+# c = [1,2,3] #リスト
+# d = {'apple': 200,'orange': 100, 'banana':150} #辞書
+#
+# #定義した変数の内容を出力してみる
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# x,y,z = 1,2,3
+# a,b,c = x,y,z
+#
+# print(a)
+# print(b)
+# print(c)
+# bin_val = 0b0101
+# print(bin_val)
+
+x = 0.0012
+print(x)
+
+
+
+
+
